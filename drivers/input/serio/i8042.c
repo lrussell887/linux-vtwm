1eb6c789953163cdd0a55e7c5d9b871031b0ba63
Tony Prisk <linux@prisktech.co.nz>
2014-01-18 11:22:43 +1300

    input: i8042: Add DT support and vt8500/wm8505 device tree nodes
    This adds devicetree support for the i8042 controller driver and wires
    it up for the vt8500 and wm8505 platforms.
    
    Additionally, this commit includes the device tree node for the VIA
    Velocity network adapter on the WM8505.
    
    Co-developed-by: Alexey Charkov <alchark@gmail.com>
    Signed-off-by: Alexey Charkov <alchark@gmail.com>
    [Logan Russell: Squashed historical patches for history compaction]
    Signed-off-by: Logan Russell <me@lrussell.net>

--- drivers/input/serio/i8042.c
+++ drivers/input/serio/i8042.c
@@ -1547,6 +1547,10 @@ static int i8042_probe(struct platform_device *dev)
 {
 	int error;
 
+#ifdef CONFIG_SERIO_I8042_DT
+	i8042_platform_device = dev;
+#endif
+
 	if (i8042_reset == I8042_RESET_ALWAYS) {
 		error = i8042_controller_selftest();
 		if (error)
@@ -1595,12 +1599,18 @@ static void i8042_remove(struct platform_device *dev)
 	i8042_controller_reset(false);
 }
 
+static struct of_device_id i8042_dt_ids[] = {
+	{ .compatible = "intel,8042" },
+	{ /* Sentinel */ },
+};
+
 static struct platform_driver i8042_driver = {
 	.driver		= {
 		.name	= "i8042",
 #ifdef CONFIG_PM
 		.pm	= &i8042_pm_ops,
 #endif
+		.of_match_table = i8042_dt_ids,
 	},
 	.probe		= i8042_probe,
 	.remove_new	= i8042_remove,
@@ -1632,6 +1642,7 @@ static int __init i8042_init(void)
 	if (err)
 		goto err_platform_exit;
 
+#ifndef CONFIG_SERIO_I8042_DT
 	i8042_platform_device = platform_device_alloc("i8042", -1);
 	if (!i8042_platform_device) {
 		err = -ENOMEM;
@@ -1641,16 +1652,19 @@ static int __init i8042_init(void)
 	err = platform_device_add(i8042_platform_device);
 	if (err)
 		goto err_free_device;
+#endif
 
 	bus_register_notifier(&serio_bus, &i8042_kbd_bind_notifier_block);
 	panic_blink = i8042_panic_blink;
 
 	return 0;
 
+#ifndef CONFIG_SERIO_I8042_DT
 err_free_device:
 	platform_device_put(i8042_platform_device);
 err_unregister_driver:
 	platform_driver_unregister(&i8042_driver);
+#endif
  err_platform_exit:
 	i8042_platform_exit();
 	return err;
